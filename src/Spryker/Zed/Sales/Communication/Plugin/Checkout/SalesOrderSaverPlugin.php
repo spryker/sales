c2da7d968d79662cfaf43852ccd700d3966b7a9f
Denis Turkov <denis.turkov@spryker.com>
2017-11-02 16:37:21 +0000

    CORE-1296: Refactor doSave plugins to use a SaveOrderTransfer directly and do not change the checkout response

--- src/Spryker/Zed/Sales/Communication/Plugin/Checkout/SalesOrderSaverPlugin.php
+++ src/Spryker/Zed/Sales/Communication/Plugin/Checkout/SalesOrderSaverPlugin.php
@@ -5,17 +5,15 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Zed\Sales\Communication\Plugin;
+namespace Spryker\Zed\Sales\Communication\Plugin\Checkout;
 
 use Generated\Shared\Transfer\CheckoutResponseTransfer;
 use Generated\Shared\Transfer\QuoteTransfer;
-use Spryker\Zed\Checkout\Dependency\Plugin\CheckoutSaveOrderInterface;
+use Generated\Shared\Transfer\SaveOrderTransfer;
+use Spryker\Zed\Checkout\Dependency\Plugin\PlaceOrder\CheckoutSaveOrderInterface;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
 
 /**
- * @deprecated Use \Spryker\Zed\Sales\Communication\Plugin\Checkout\SalesOrderSaverPlugin instead.
- * Will be removed with the next major.
- *
  * @method \Spryker\Zed\Sales\Business\SalesFacade getFacade()
  * @method \Spryker\Zed\Sales\Communication\SalesCommunicationFactory getFactory()
  */
@@ -25,12 +23,12 @@ class SalesOrderSaverPlugin extends AbstractPlugin implements CheckoutSaveOrderI
      * @api
      *
      * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
-     * @param \Generated\Shared\Transfer\CheckoutResponseTransfer $checkoutResponse
+     * @param SaveOrderTransfer $saveOrderTransfer
      *
      * @return void
      */
-    public function saveOrder(QuoteTransfer $quoteTransfer, CheckoutResponseTransfer $checkoutResponse)
+    public function saveOrder(QuoteTransfer $quoteTransfer, SaveOrderTransfer $saveOrderTransfer)
     {
-        $this->getFacade()->saveOrder($quoteTransfer, $checkoutResponse);
+        $this->getFacade()->saveSalesOrder($quoteTransfer, $saveOrderTransfer);
     }
 }
