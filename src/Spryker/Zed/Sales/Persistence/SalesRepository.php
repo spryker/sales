ac9c2d42ad727ead285c912502b32c31b50760a5
Eugenia Poidenko <eugenia.poidenko@spryker.com>
2019-09-11 09:45:28 +0300

    GLUE-5578 some renamings.

--- src/Spryker/Zed/Sales/Persistence/SalesRepository.php
+++ src/Spryker/Zed/Sales/Persistence/SalesRepository.php
@@ -61,6 +61,6 @@ class SalesRepository extends AbstractRepository implements SalesRepositoryInter
 
         return $this->getFactory()
             ->createSalesOrderMapper()
-            ->mapPaginatedOrderListTransfer($orderListTransfer, $orderListQuery->find()->getArrayCopy(), $ordersCount);
+            ->mapSalesOrderEntitiesToOrderListTransfer($orderListQuery->find()->getArrayCopy(), $orderListTransfer, $ordersCount);
     }
 }
